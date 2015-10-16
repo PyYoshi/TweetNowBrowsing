186982205df3112870dce1e3b75d85c3b6dc9002
PyYoshi <myoshi321go@gmail.com>
2015-10-16 18:30:16 +0900

    delete say_hello scripts

--- src/js/content_scripts/say_hello/app.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-import SEND_MESSAGE_ORDER_SAY_HELLO from 'common/const';
-
-chrome.runtime.sendMessage({'order': SEND_MESSAGE_ORDER_SAY_HELLO});
--- src/js/content_scripts/say_hello/main.js
+++
@@ -1,2 +0,0 @@
-System.baseURL = chrome.extension.getURL('/');
-System.import('content_scripts/say_hello/app');
