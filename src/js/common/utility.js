8f8ff21e51f8c41a80b9a52233cd3a110d61fbf5
PyYoshi <myoshi321go@gmail.com>
2015-10-17 14:31:13 +0900

    GUIDを生成する関数を追加

--- src/js/common/utility.js
+++ src/js/common/utility.js
@@ -2,6 +2,17 @@
 
 class Utility {
 
+    /**
+     * GUIDを生成する
+     * http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
+     * @return {String}
+     */
+    static guid() {
+        function s4() {
+            return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+        }
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+    }
 }
 
 export default Utility;
