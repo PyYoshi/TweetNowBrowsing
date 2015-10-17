369a7016802117bb35f1f1ed8d56e78a8297e1c5
PyYoshi <myoshi321go@gmail.com>
2015-10-17 14:31:29 +0900

    localStorageを操作するためのクラスを追加

---
+++ src/js/common/localstorage.js
@@ -0,0 +1,30 @@
+'use strict';
+
+class LocalStorage {
+
+    /**
+     * localStorageから値を取得
+     * @param {String} key localStorageのkey
+     * @param {Number|String|Boolean|object|array} 初期値
+     * @return {Number|String|Boolean|object|array|null}
+     */
+    static get(key, defaultValue = null) {
+        let value = window.localStorage[key];
+        if (typeof value === 'undefined') {
+            return defaultValue;
+        } else {
+            return JSON.parse(value);
+        }
+    }
+
+    /**
+     * localStorageへ値をセット
+     * @param {String} key localStorageのkey
+     * @param {Number|String|Boolean|object|array}
+     */
+    static set(key, value) {
+        window.localStorage[key] = JSON.stringify(value);
+    }
+}
+
+export default LocalStorage;
