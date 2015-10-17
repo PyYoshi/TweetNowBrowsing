0bf8a4393f8091c06f93f48f5f4dc2b30013dc6d
PyYoshi <myoshi321go@gmail.com>
2015-10-17 14:30:58 +0900

    SEND_MESSAGE_ORDER_COLLECT_TIWPがundefinedだった問題を修正

--- src/js/content_scripts/collect_tiwp/app.js
+++ src/js/content_scripts/collect_tiwp/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import SEND_MESSAGE_ORDER_COLLECT_TIWP from 'common/const';
+import {SEND_MESSAGE_ORDER_COLLECT_TIWP} from 'common/const';
 
 // Tweet Intent WebページのHTML解析処理を走らせる
 chrome.runtime.sendMessage({'order': SEND_MESSAGE_ORDER_COLLECT_TIWP});
