fee3bcaffc61ffb3204805aa7771e52bd4098d02
PyYoshi <myoshi321go@gmail.com>
2015-10-18 12:36:46 +0900

    jspm install jquery

--- src/js/lib/jspm.config.js
+++ src/js/lib/jspm.config.js
@@ -25,6 +25,7 @@ System.config({
     "chai": "npm:chai@3.3.0",
     "core-js": "npm:core-js@1.2.1",
     "css": "github:systemjs/plugin-css@0.1.18",
+    "jquery": "npm:jquery@2.1.4",
     "lodash": "npm:lodash@3.10.1",
     "mocha": "npm:mocha@2.3.3",
     "superagent": "npm:superagent@1.4.0",
@@ -322,6 +323,9 @@ System.config({
     },
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:jquery@2.1.4": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:lodash@3.10.1": {
       "process": "github:jspm/nodelibs-process@0.1.2"
