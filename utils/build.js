81a3c4eb0f49df7aefeb97eef9f6fa5a93ed7f49
PyYoshi <myoshi321go@gmail.com>
2017-12-19 12:11:03 +0900

    release.zipを生成するように

--- utils/build.js
+++ utils/build.js
@@ -1,8 +1,36 @@
 var webpack = require('webpack'),
-    config = require('../webpack.config');
+  env = require('./env'),
+  config = require('../webpack.config'),
+  fs = require('fs'),
+  path = require('path'),
+  rimraf = require('rimraf'),
+  archiver = require('archiver');
 
 delete config.chromeExtensionBoilerplate;
 
 webpack(config, function(err) {
-    if (err) throw err;
+  if (err) {
+    throw err;
+  }
+
+  if (env.NODE_ENV === 'production') {
+    const zipPath = path.join(__dirname, '../', 'release.zip');
+
+    rimraf(zipPath, (rimrafErr) => {
+      if (rimrafErr) {
+        throw rimrafErr;
+      }
+
+      const output = fs.createWriteStream(zipPath);
+      const zip = archiver('zip', {
+        zlib: { level: 9 }
+      });
+      zip.pipe(output);
+      zip.on('error', function(zipErr) {
+        throw zipErr;
+      });
+
+      zip.directory(path.join(__dirname, '../', 'build'), false).finalize();
+    });
+  }
 });
