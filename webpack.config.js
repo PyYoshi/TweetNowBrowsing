4b3ca49fcd2f24e598d15fc07234517cf57d3381
PyYoshi <myoshi321go@gmail.com>
2020-04-21 10:29:05 +0900

    typescriptでwebpack buildできるようにする

--- webpack.config.js
+++ webpack.config.js
@@ -17,9 +17,9 @@ module.exports = {
         test: /\.js(\?.*)?$/i,
         terserOptions: {
           mangle: false,
-          sourceMap: process.env.NODE_ENV !== 'production' ? true : false
-        }
-      })
+          sourceMap: process.env.NODE_ENV !== 'production' ? true : false,
+        },
+      }),
     ],
     runtimeChunk: false,
     splitChunks: {
@@ -27,39 +27,53 @@ module.exports = {
         vendor: {
           test: /[\\/]node_modules[\\/]/,
           name: 'vendor',
-          chunks: 'all'
-        }
-      }
-    }
+          chunks: 'all',
+        },
+      },
+    },
   },
   entry: {
-    popup: path.join(__dirname, 'src', 'js', 'popup', 'index.js'),
-    options: path.join(__dirname, 'src', 'js', 'options', 'index.js'),
-    background: path.join(__dirname, 'src', 'js', 'background', 'index.js'),
-    collect_tiwp: path.join(__dirname, 'src', 'js', 'content_scripts', 'collect_tiwp', 'index.js')
+    popup: path.join(__dirname, 'src', 'js', 'popup', 'index.ts'),
+    options: path.join(__dirname, 'src', 'js', 'options', 'index.ts'),
+    background: path.join(__dirname, 'src', 'js', 'background', 'index.ts'),
+    collect_tiwp: path.join(__dirname, 'src', 'js', 'content_scripts', 'collect_tiwp', 'index.ts'),
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
   },
   output: {
     path: path.join(__dirname, 'build'),
-    filename: '[name].js'
+    filename: '[name].js',
   },
   module: {
     rules: [
       {
         test: /\.css$/,
         loader: 'style-loader!css-loader',
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: new RegExp(`.(${fileExtensions.join('|')})$`),
         loader: 'file-loader?name=[name].[ext]',
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.html$/,
         loader: 'html-loader',
-        exclude: /node_modules/
-      }
-    ]
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+            },
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new CleanWebpackPlugin({}),
@@ -74,58 +88,58 @@ module.exports = {
           }
 
           return Buffer.from(JSON.stringify(manifestJSON, null, '  '));
-        }
+        },
       },
       {
         from: 'src/_locales',
-        to: '_locales'
+        to: '_locales',
       },
       {
-        from: 'LICENSE'
+        from: 'LICENSE',
       },
 
       // Fonts: Material Icons
       {
-        from: 'src/css/MaterialIcons-Regular.woff2'
+        from: 'src/css/MaterialIcons-Regular.woff2',
       },
 
       // Fonts: Roboto
       {
-        from: 'src/css/roboto-v18-latin-italic.woff2'
+        from: 'src/css/roboto-v18-latin-italic.woff2',
       },
       {
-        from: 'src/css/roboto-v18-latin-regular.woff2'
+        from: 'src/css/roboto-v18-latin-regular.woff2',
       },
 
       // muicss
       {
         from: 'src/thirdparty/mui-0.9.35/css/mui.min.css',
-        to: 'muicss'
+        to: 'muicss',
       },
       {
         from: 'src/thirdparty/mui-0.9.35/js/mui.min.js',
-        to: 'muicss'
+        to: 'muicss',
       },
       {
         from: 'src/thirdparty/mui-0.9.35/LICENSE.txt',
-        to: 'muicss'
-      }
+        to: 'muicss',
+      },
     ]),
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'src', 'popup.html'),
       filename: 'popup.html',
-      chunks: ['vendor', 'popup']
+      chunks: ['vendor', 'popup'],
     }),
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'src', 'options.html'),
       filename: 'options.html',
-      chunks: ['vendor', 'options']
+      chunks: ['vendor', 'options'],
     }),
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'src', 'background.html'),
       filename: 'background.html',
-      chunks: ['vendor', 'background']
+      chunks: ['vendor', 'background'],
     }),
-    new WriteFilePlugin()
-  ]
+    new WriteFilePlugin(),
+  ],
 };
